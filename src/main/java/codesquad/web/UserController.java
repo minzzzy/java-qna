6ade5531087a9b11ab81ef74013d24f2c88e9fef
minzzzy <minpjh@gmail.com>
2018-07-09 14:10:18 +0900

    Remvoe duplicated html code in index and add request mapping

--- src/main/java/codesquad/web/UserController.java
+++ src/main/java/codesquad/web/UserController.java
@@ -6,27 +6,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
     private List<User> users = new ArrayList<>();
 
-    @PostMapping("/users")
+    @PostMapping("")
     public String create(User user) {
         users.add(user);
         return "redirect:/users";
     }
 
-    @GetMapping("/users")
+    @GetMapping("")
     public String list(Model model) {
         model.addAttribute("users", users);
         return "/user/list";
     }
 
-    @GetMapping("/users/{index}")
+    @GetMapping(":{index}")
     public String show(@PathVariable int index, Model model) {
         model.addAttribute("user", users.get(index));
         return "/user/profile";
