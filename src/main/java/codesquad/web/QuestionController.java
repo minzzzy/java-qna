945ff3aa8e87a0be402cec6dbeb69d687ce27593
minzzzy <minpjh@gmail.com>
2018-07-09 12:06:34 +0900

    Remove duplicated html code for

--- src/main/java/codesquad/web/QuestionController.java
+++ src/main/java/codesquad/web/QuestionController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 @Controller
